7b24dc8e72a41866fd943501e7ffef1a1a674651
Tamires Cavalcante <69002877+tamiresCavalcante@users.noreply.github.com>
2020-11-06 15:12:26 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,14 @@
+function getName(){
+    return 'Tamires Cavalcante';
+}
+
+//console.log(getName());
+
+function logFn(fn){
+    console.log(fn());
+}
+
+logFn(getName);
+//const logFnResult = logFn;
+
+//logFnResult(getName);
